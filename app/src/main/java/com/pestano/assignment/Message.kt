5b7c4b4dfff23696ad4d8e2b6c78aee933bfeb58
borisboeri <boris.boeri@gmail.com>
2019-01-27 20:03:46 +0100

    add identity

--- app/src/main/java/com/pestano/assignment/Message.kt
+++ app/src/main/java/com/pestano/assignment/Message.kt
@@ -3,10 +3,10 @@ package com.pestano.assignment
 import com.firebase.ui.auth.data.model.User
 import java.util.*
 
-data class Message {
+data class Message (
     var title: String = "",
     var content: String = "",
-    var author: User_profile = null,
-    var creation_date: Date = null,
+    var author: User_profile? = null,
+    var creation_date: Date? = null,
     var category: String = ""
-}
+)
